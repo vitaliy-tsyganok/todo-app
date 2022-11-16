43d516b36dc7e05cf08e3c4c1261593610057507
Vitaliy Tsyganok <vitalytsiganok@gmail.com>
2022-11-16 09:42:17 +0400

    Code style

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ renderTodos()
 addTodoFormNode.addEventListener('submit', addTodoFormNodeHandler)
 // addTodoFormNode.input.addEventListener('keydown', keydownEnter)
 
-
 // Handlers
 function addTodoFormNodeHandler(event) {
 	event.preventDefault()
@@ -43,22 +42,25 @@ function renderUsers() {
 	let html = '<option value="">Select user</option>'
 	downloadUsers()
 		.then(() => {
-			users.forEach(user => {
+			users.forEach((user) => {
 				html += `<option value="${user.id}">${user.name}</option>`
 			})
 		})
 		.then(() => {
 			console.log(users)
 			document.querySelector('.addTodo__selectUser').innerHTML = html
-		}
-		)
+		})
 }
 
 function renderTodos() {
 	let html = ''
 	// console.log('renderTodos', users)
 	todos.forEach((todo) => {
-		html += `<li class="${todo.isDone ? 'isDone' : ''}" data-id="${todo.id}"><div>${todo.todoText} <i>by ${getUser(todo.todoBody.userId)?.name}</i></div>
+		html += `<li class="${todo.isDone ? 'isDone' : ''}" data-id="${
+			todo.id
+		}"><div>${todo.todoText} <i>by ${
+			getUser(todo.todoBody.userId)?.name
+		}</i></div>
 			<button onClick="setIsDone(this)">Done</button>
 			<button onClick="deleteTodo(this)">Delete</button>
 		</li>`
@@ -72,22 +74,22 @@ function addTodo(todoText, userId) {
 		isDone: false,
 		id: `${Math.random()}`,
 		todoBody: {
-			userId
-		}
+			userId,
+		},
 	}
 	todos = [...todos, todo]
 }
 
 function deleteTodo(elem) {
 	const id = getTodoId(elem)
-	todos = todos.filter(todo => todo.id !== id)
+	todos = todos.filter((todo) => todo.id !== id)
 	renderTodos()
 	saveToLocalStorage('todos', todos)
 }
 
 function setIsDone(elem) {
 	const id = getTodoId(elem)
-	todos.forEach(todo => {
+	todos.forEach((todo) => {
 		if (todo.id === id) {
 			todo.isDone = !todo.isDone
 		}
@@ -109,14 +111,14 @@ function getTodoUserId() {
 }
 
 function getUser(userId) {
-	return users.find(user => user.id === userId)
+	return users.find((user) => user.id === userId)
 }
 
 function removeTodoText() {
 	addTodoFormNode.input.value = ''
 }
 
-// Buttons action 
+// Buttons action
 function clearTodosAll() {
 	todos = []
 	renderTodos()
@@ -124,39 +126,42 @@ function clearTodosAll() {
 }
 
 function clearTodosDone() {
-	todos = todos.filter(todo => !todo.isDone)
+	todos = todos.filter((todo) => !todo.isDone)
 	renderTodos()
 	saveToLocalStorage('todos', todos)
 }
 
 function downloadTasks() {
-	downloadTasksPromise()
-		.then(downloadedTodos => {
-			[...downloadedTodos] = downloadedTodos.map(todo => {
-				const { completed: isDone, title: todoText, } = todo
-				return {
-					id: `${Math.random()}`,
-					isDone,
-					todoText,
-					todoBody: {
-						...todo
-					}
-				}
-			})
-			todos = [...todos, ...downloadedTodos]
-			renderTodos()
-			saveToLocalStorage('todos', todos)
+	downloadTasksPromise().then((downloadedTodos) => {
+		;[...downloadedTodos] = downloadedTodos.map((todo) => {
+			const { completed: isDone, title: todoText } = todo
+			return {
+				id: `${Math.random()}`,
+				isDone,
+				todoText,
+				todoBody: {
+					...todo,
+				},
+			}
 		})
+		todos = [...todos, ...downloadedTodos]
+		renderTodos()
+		saveToLocalStorage('todos', todos)
+	})
 }
 
 async function downloadTasksPromise() {
-	const data = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=7')
+	const data = await fetch(
+		'https://jsonplaceholder.typicode.com/todos?_limit=7'
+	)
 	const downloadedTodos = await data.json()
 	return downloadedTodos
 }
 
 async function downloadUsers() {
-	const data = await fetch('https://jsonplaceholder.typicode.com/users?_limit=7')
-	const downloadedUsers = await data.json()	
+	const data = await fetch(
+		'https://jsonplaceholder.typicode.com/users?_limit=7'
+	)
+	const downloadedUsers = await data.json()
 	users = [...downloadedUsers]
 }
